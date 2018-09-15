fcc6c36d69ae6e6662a699d42fb6da369060e9b3
Yoel Ramos Castillo <yoel.castillo@admobilize.com>
2018-09-15 10:17:40 -0400

    adding dataset loader script

---
+++ 6-create-your-own-classifier-project/setup-dataset.bash
@@ -0,0 +1,4 @@
+# cd /6-create-your-own-classifier-project
+wget -nc https://s3.amazonaws.com/content.udacity-data.com/nd089/flower_data.tar.gz
+tar -xzf flower_data.tar.gz
+# rm flower_data.tar.gz
---
+++ 6-create-your-own-classifier-project/train.py
@@ -0,0 +1,17 @@
+# Yoel Ramos 
+# yoelrc88@gmail.com
+
+import time
+import torch
+from torch import nn
+from torch import optim
+import torchvision
+from torchvision import datasets, transforms, models
+from collections import OrderedDict
+import json
+from PIL import Image
+from torch.autograd import Variable
+import numpy as np
+import matplotlib.pyplot as plt
+
+
